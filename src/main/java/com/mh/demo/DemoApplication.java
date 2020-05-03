26dd9620fee7f4cc85598694ec266cabf45858f9
Mahfuzul Haque <mahfuzul.haque@gmail.com>
2020-05-04 06:52:01 +1000

    Returning object

--- src/main/java/com/mh/demo/DemoApplication.java
+++ src/main/java/com/mh/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.mh.demo;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+	
+	private final AtomicLong counter = new AtomicLong();
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
-	@GetMapping("/hello2")
-	public String hello2(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		String message = String.format("Hello %s!", name);
+		Greeting greeting = new Greeting(counter.incrementAndGet(), message); 
+		
+		return greeting;
 	}
 
 }
